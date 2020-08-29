0ac51d6dc4e4db3e83601d048db4baa5f53ca9b4
ruruu127 <69493277+ruruu127@users.noreply.github.com>
2020-08-30 00:29:52 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -3,18 +3,16 @@ library(dplyr)
 # Read in data
 d <- readRDS("summarySCC_PM25.rds")
 
-# Plot 2: Total emissions in Baltimore City
-png("plot2.png", width = 480, height = 480)
-baltimore.emission <- summarise(group_by(filter(d, fips == "24510"), year), 
-                                Emissions=sum(Emissions))
+# Plot 1: Total emissions in the US
+png("plot1.png", width = 480, height = 480)
+tot.Emission <- summarise(group_by(d, year), Emissions=sum(Emissions))
+tot.Emission$Emissions <- tot.Emission$Emissions/1000
 colors <- as.character(rainbow(4))
-p2 <- barplot(baltimore.emission$Emissions, 
-              names.arg = baltimore.emission$year, 
-              col = colors, xlab = "Year", 
-              ylab = "Total Emissions (kilotons)",
-              main = "Total PM2.5 Emissions in Baltimore City, Maryland", 
-              ylim = c(0,4000))
-text(x = p2, y = baltimore.emission$Emissions, 
-     label = round(baltimore.emission$Emissions, 3),
+p1 <- barplot(tot.Emission$Emissions, names.arg = tot.Emission$year, 
+             col = colors, xlab = "Year", 
+             ylab = "Total Emissions (kilotons)",
+             main = "Total PM2.5 Emissions in the US", ylim = c(0,8000))
+text(x = p1, y = tot.Emission$Emissions, 
+     label = round(tot.Emission$Emissions, 3),
      pos = 3, cex = 0.8, col = "black")
 dev.off()
